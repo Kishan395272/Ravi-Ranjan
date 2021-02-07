ebe54c385da32b132a1eb6823ce3ca8380bdf2df
Kishan395272 <76143151+Kishan395272@users.noreply.github.com>
2021-02-07 20:09:29 +0530

    Update add_with_4_functions.c

--- add_with_4_functions.c
+++ add_with_4_functions.c
@@ -1,1 +1,36 @@
 //Write a program to add two user input numbers using 4 functions.
+
+
+
+#include<stdio.h>
+
+int first_num()
+{
+int num;
+printf("Enter first number");
+scanf("%d",&num);
+return num;
+}
+int second_num()
+{
+int num;
+printf("Enter second number");
+scanf("%d",&num);
+return num;
+}
+int add_two_num(int first_num,int second_num)
+{
+return (first_num+second_num);
+}
+int print(int sum)
+{
+printf("The sum of two number is=%d\n",sum);
+}
+int main()
+{
+int x=first_num();
+int y=second_num();
+int sum=add_two_num(x,y);
+print(sum);
+return 0;
+}
