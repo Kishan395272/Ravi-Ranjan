8d16f4aac4503cf38448a155203250db234de12f
Kishan395272 <76143151+Kishan395272@users.noreply.github.com>
2021-02-07 11:44:56 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -1,1 +1,16 @@
-//Write a program to add two user input numbers using one function.
+//Write a program to add two user input numbers using one function.#include<stdio.h>
+int main()
+{
+int a,b,x;
+printf("Enter number 1\n");
+scanf("%d",&a);
+printf("Enter number2\n");
+scanf("%d",&b);
+
+x=a+b;
+printf("sum of %d and %d is =%d\n",a,b,x);
+return 0;
+}
+
+
+
