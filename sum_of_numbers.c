9972c2ba2b62ff5ddcb22f065caf63349837d7ec
Kishan395272 <76143151+Kishan395272@users.noreply.github.com>
2021-02-09 22:05:41 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -3,15 +3,16 @@
 #include<stdio.h>
 int main()
 {
-int n,sum=0,i,value;
-printf("Enter the number  user want to add:\n");
+// for finding sum of different numbers we will take 4 values that is
+int i,sum=0,n,value;
+printf("Enter the number user want to add:\n");
 scanf("%d",&n);
 printf("Enter %d numbers\n",n);
-for (i=1;i<=n;i++)
+for(i=1;i<=n;i++)
 {
 scanf("%d",&value);
 sum=sum+value;
 }
-printf("sum of entered number is %d",sum);
+printf("total sum of entered number is %d",sum);
 return 0;
 }
