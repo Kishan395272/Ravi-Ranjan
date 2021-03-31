bb0b47fb4b6daabdcbaec11e6b360a9ac689321c
Kishan395272 <76143151+Kishan395272@users.noreply.github.com>
2021-04-01 00:50:49 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,24 +1,38 @@
 //WAP to find the distance between two points using structures and 4 functions.
 
-#include<stdio.h>
+
+
+
+
 #include<math.h>
-int main()
+#include<stdio.h>
+struct distance
 {
-int x1,x2,y1,y2,x,y,distance;
 
-//Initial and final points we take to find distance between them
+float x,y;
+};
+ 
+float distance(struct distance p,struct distance q)
+    
 
-printf("Enter initial points for both coordinates:");
-scanf("%d%d",&x1,&y1);
-
-printf("Enter final points for both coordinates:");
-scanf("%d%d",&x2,&y2);
-
-x=(x2-x1);
-y=(y2-y1);
- distance= sqrt(x*x + y*y);
-
-printf("Distance between two points is %d",distance);
+{
+float w;
+w=sqrt(((p.x-q.x)*(p.x-q.x))+((p.y-q.y)*(p.y-q.y)));
+return w;
+ int main()
+{
+float result;
+struct distance p,q;
+printf("Enter the x cordinate of point p:");
+scanf("%f",&p.x);
+printf("Enter the x cordinate of point q:");
+scanf("%f",&q.x);
+printf("Enter the y cordinate of point p:");
+scanf("%f",&p.y);
+printf("Enter the y cordinate of point q:");
+scanf("%f",&q.y);
+result=distance(p,q);
+printf("the distance between 2 point is =%f",result);
 return 0;
 }
 
