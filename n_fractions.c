7a334c96f99c4bfe98a5e25eebc0de9c1a3d7ba7
Kishan395272 <76143151+Kishan395272@users.noreply.github.com>
2021-04-15 13:52:41 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,27 +1,84 @@
 //WAP to find the sum of n fractions.
 
+
+
+
+
 #include<stdio.h>
+
+struct frac
+{
+int n,d;   //n=numerator,d=denominator
+};
+struct frac input()
+{ 
+struct frac x;
+printf("put values for numerator and denominator\n");
+scanf("%d%d",&x.n,&x.d);
+return x;
+}
+int i;
+void input_n(int n,struct frac arr[50])
+{
+for(int i=0;i<n;i++);
+ {
+  arr[i]=input();
+ }
+}
+int gcd(int p,int q)
+{
+int i,j;
+j=1;
+for(i=2;i<=p && i<=q;i++)
+{
+  if(p%i==0 && q%i==0)
+   i=j;
+}
+return  j;
+}
+struct frac compute(struct frac p,struct frac q)
+{
+struct frac sum;
+sum.n=(p.n*q.d)+(p.d*q.n);
+sum.d=(p.d*q.d);
+int Gcd=gcd(sum.n,sum.d);
+sum.n=(sum.n/Gcd);
+sum.d=(sum.d/Gcd);
+return sum;
+}
+struct frac compute_n(int n,struct frac arr[50])
+{
+struct frac add;
+add.n=0;
+add.d=1;
+for(int i=0;i<n;i++)
+{
+add=compute(add,arr[i]);
+}
+return add;
+}
+
+void display(int n,struct frac arr[50],struct frac sum)
+{
+for(int i=0;i<n;i++)
+{
+printf("%d / %d\n",arr[i].n,arr[i].d);
+}
+printf("%d/%d",sum.n,sum.d);
+}
 int main()
 {
-float a,b,c,d,w,x,z;
-printf("Put the value of 1st numerator:\n");
-scanf("%f",&a);
-printf("put the value of 2nd numerator:\n");
-scanf("%f",&b);
-printf("Put the value of 1st denominator:\n");
-scanf("%f",&c);
-printf("put the value of 2nd denominator:\n");
-scanf("%f",&d);
+int n;
 
-//To add two number
- 
-w=(a*d)+ (c*b);
-x=(c*d);
-z=w/x;
-
-printf("the value of z will be %f\n",z);
+struct frac arr[50],sum;
+printf("enter number of fraction you want to add\n");
+scanf("%d",&n);
+input_n(n,arr);
+sum=compute_n(n,arr);
+display(n,arr,sum);
 return 0;
 }
+
 
 
 
